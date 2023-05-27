4f61410c1efba36256da8f531229cad2f34b6c60
nykoh2001 <nykoh2001@gmail.com>
2023-05-28 01:27:23 +0900

    feat: set/install solc with parsed file version

--- find_vul.py
+++ find_vul.py
@@ -1,12 +1,17 @@
 import argparse
 import logging
 from solidity_parser.parser import parse
-from solcx import get_available_solc_versions
+from solcx import (
+    get_available_solc_versions,
+    set_solc_version_pragma,
+    compile_files,
+    install_solc_pragma,
+)
+from solcx.exceptions import SolcNotInstalled
 
 
 def parse_file(file):
     parsed_file = parse(file.read().decode("utf-8"))
-    print("parse:", parsed_file)
     for children in parsed_file["children"]:
         if children["type"] == "PragmaDirective":
             return children["value"]
@@ -26,7 +31,18 @@ def main():
     logger.info(f"analyzing {filename}")
 
     byte_code = {}
-    parsed_file = parse_file(file)
+    parsed_file_version = parse_file(file)
+    try:
+        set_solc_version_pragma(parsed_file_version)
+    except SolcNotInstalled:
+        logger.info(f"Installing solc version {parsed_file_version}...")
+        install_solc_pragma(parsed_file_version)
+        set_solc_version_pragma(parsed_file_version)
+        logger.info("Installed")
+
+    contracts = compile_files([filename])
+    logger.info(f"compiled {filename}")
+    print("contracts:", contracts)
 
 
 if __name__ == "__main__":
