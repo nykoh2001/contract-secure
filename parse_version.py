1658bf9d14b73fd48c4c5c09a20393e821401043
nyk <nykoh2001@gmail.com>
2023-06-03 14:56:44 +0900

    feat: parse and set solc version

---
+++ parse_version.py
@@ -0,0 +1,21 @@
+from solcx import (set_solc_version_pragma, install_solc_pragma,
+                   get_available_solc_versions,)
+from solidity_parser.parser import parse
+from solcx.exceptions import SolcNotInstalled
+
+
+def parse_file(file):
+    parsed_file = parse(file.read().decode("utf-8"))
+    for children in parsed_file["children"]:
+        if children["type"] == "PragmaDirective":
+            return children["value"]
+    return get_available_solc_versions()[0]
+
+
+def parse_version(file):
+    parsed_file_version = parse_file(file)
+    try:
+        set_solc_version_pragma(parsed_file_version)
+    except SolcNotInstalled:
+        install_solc_pragma(parsed_file_version)
+        set_solc_version_pragma(parsed_file_version)
