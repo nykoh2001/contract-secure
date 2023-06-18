17917af54e9a5dbb0873e15ff3b6df594a8b19fc
nyk <nykoh2001@gmail.com>
2023-06-19 00:10:01 +0900

    feat: main func

--- find_vul.py
+++ find_vul.py
@@ -4,12 +4,12 @@ from solcx import (
     compile_files,
 )
 from parse_version import parse_version
-from solcx.install import get_executable
 
-from disassembler.disassem import EVMDisassemble
+from rattle.evmasm import EVMAsm
+from rattle.recover import Recover
+from symbolic_exe import Symbolic
 
-
-logger = logging.getLogger()
+from add_overflow import check_add_overflow
 
 
 def main():
@@ -20,14 +20,12 @@ def main():
     args = arg_parser.parse_args()
     file = args.file
     filename = file.name
-    logger.info(f"analyzing {filename}")
 
     byte_code = {}
 
     parse_version(file)
 
     contracts = compile_files([filename])
-    logger.info(f"compiled {filename}")
 
     for name, c in contracts.items():
         bin_runtime = c['bin-runtime']
@@ -38,11 +36,24 @@ def main():
     print("byte code:", byte_code)
 
     for name, byte in byte_code.items():
-        logger.info(f"Analyzing {name}")
         print("evm disassembled: ")
-        for r in EVMDisassemble.disassem_evm(byte):
+        for r in EVMAsm.disassemble_all(byte):
             print(r.name, r.opcode, r.operand)
         # disassemble evm bytecode
+        recover = Recover(byte, edges=[], optimize=True)
+        sym = Symbolic(recover)
+        traces = sym.run()
+        print("traces:", traces)
+        for trace in traces:
+            for block in trace.blocks_checked:
+                for inst in block.block.insns:
+                    inst_name = inst.insn.name
+                    # registers = block.state.registers
+                    args = inst.arguments
+                    print("instruction name:", inst_name)
+                    if inst_name == "ADD":
+                        print("check integer overflow...")
+                        check_add_overflow(inst, block.block)
 
 
 if __name__ == "__main__":
