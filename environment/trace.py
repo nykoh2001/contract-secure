baa3a9efaecd2d0e233d1f58ff7651ab98226036
nyk <nykoh2001@gmail.com>
2023-06-19 11:20:06 +0900

    feat: trace environment

--- environment/trace.py
+++ environment/trace.py
@@ -2,15 +2,13 @@ from environment.state import State
 
 
 class Trace:
-    def __init__(self, block=None, state=None, environment=None, contraints=None, block_checked=[]):
+    def __init__(self, block=None, state=State(), environment=None, contraints=None, block_checked=[]):
         self.blocks_checked = block_checked
         self.state = state
         self.environment = environment
         self.current_constraint = None
         self.constraints = contraints
         self.block = block
-        if state == None:
-            self.state = State()
 
     def add_checked_block(self, block):
         self.blocks_checked.append(block)
